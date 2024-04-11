c52de11b303e5a9919ce8e2f5e89ffa693f6f44f
Anmol_sankadiya <anmolsankadiya.as@gmail.com>
2024-04-11 12:14:35 +0530

    Creating the controller for post api

---
+++ inventoryController.js
@@ -0,0 +1,20 @@
+const Inventory = require('./inventoryModel');
+
+class InventoryController {
+    constructor() {
+        this.inventoryModel = new Inventory();
+    }
+
+    async createInventory(req, res) {
+        try {
+            const InventoryModel = this.inventoryModel.getModel();
+            const inventoryData = req.body;
+            const newInventory = new InventoryModel(inventoryData);
+            await newInventory.save();
+            res.status(201).json(newInventory);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    }
+}
+module.exports = InventoryController;
