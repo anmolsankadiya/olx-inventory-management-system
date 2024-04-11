0418d764a40f9c871575e837d59c79d0b3447048
Anmol_sankadiya <anmolsankadiya.as@gmail.com>
2024-04-11 12:29:47 +0530

    Importing routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+
 
 class Server {
     constructor() {
