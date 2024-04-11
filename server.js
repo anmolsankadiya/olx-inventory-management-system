002161dbd0062c3a7f5390f144d0c9d321aa264b
Anmol_sankadiya <anmolsankadiya.as@gmail.com>
2024-04-11 12:06:19 +0530

    Connecting to database

--- server.js
+++ server.js
@@ -15,3 +15,22 @@ class Server {
 
         this.startServer();
     }
+
+    connectToDB() {
+        mongoose.connect('mongodb+srv://anmolsankadiya:anmol@cluster0.y6u1lez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+    }
+
+    startServer() {
+        this.app.listen(this.PORT, () => {
+            console.log(`Server is running on port ${this.PORT}`);
+        });
+    }
+}
+
+const server = new Server();
