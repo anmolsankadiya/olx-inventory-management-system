ea0384817b8d79c28fd888f2a586726ca5026588
Anmol_sankadiya <anmolsankadiya.as@gmail.com>
2024-04-17 10:42:07 +0530

    Making a route class

--- routes.js
+++ routes.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const InventoryController = require('./inventoryController');
+import express from 'express';
+import InventoryController from '../controller/inventoryController.js';
 
-const router = express.Router();
-const inventoryController = new InventoryController();
+class Routes{
 
+    initializeRouter(){
+        const router=express.Router();
 
-router.post('/inventory', inventoryController.createInventory.bind(inventoryController));
+        const inventoryController = new InventoryController()
 
-router.put('/inventory/:sku', inventoryController.updateInventory.bind(inventoryController));
+        router.post('/inventory', inventoryController.createInventory.bind(inventoryController));
+        router.put('/inventory/update', inventoryController.updateInventory.bind(inventoryController));
+        router.delete('/inventory/delete',inventoryController.deleteInventory.bind(inventoryController));
+        router.get('/', inventoryController.getInventory.bind(inventoryController));
 
+        return router;
+    }
+}
 
-router.delete('/inventory/:sku', inventoryController.deleteInventory.bind(inventoryController));
-module.exports = router;
+const routes=new Routes();
+export default routes;
